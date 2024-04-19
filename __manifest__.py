6157255605c73453e5668e20417400e729c7882c
gibeongideon <kipngeno.gibeon@gmail.com>
2024-04-19 15:19:37 +0300

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+
+
+{
+    "name": "Darius Propery Management System",
+
+    'summary': """
+        Extend 
+                        """,
+
+    'description': """This technical module allows to define .""",
+    # 'category': 'Sales/Sales',
+    'version': '17.0',
+    # 'installable': True,
+
+
+   
+    "depends": [
+        "base",
+        "web",
+    ],
+
+    "data": [
+        # "security/ir.model.access.csv",
+        # "views/estate_property_offer_views.xml",
+        # "views/estate_property_tag_views.xml",
+        # "views/estate_property_type_views.xml",
+        # "views/estate_property_views.xml",
+        # "views/res_users_views.xml",
+        # "views/estate_menus.xml",
+        # "report/estate_reports.xml",
+        # "report/estate_report_views.xml",
+    ],
+    "demo": [
+        # "data/estate_demo.xml"
+    ],
+    "application": True,
+}
